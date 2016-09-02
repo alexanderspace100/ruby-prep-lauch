c973ca84748b855d4f51ca207015659a9142ea77
alexanderspace100 <alexanderspace100@gmail.com>
2016-09-02 21:56:15 +0300

    basics

---
+++ basics.rb
@@ -0,0 +1,38 @@
+puts "Alexander " + "Space"
+
+puts "." * 30
+
+puts 4567 / 1000
+puts 4567 % 1000 / 100
+puts 4567 % 1000 % 100 / 10
+puts 4567 % 1000 %100 % 10
+
+puts "." * 30
+
+movies = {:movie1 => 2000, :movie2 => 2001, :movie3 => 2002}
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+
+puts "." * 30
+
+dates = [2000, 2001, 2002]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+
+puts "." * 30
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "." * 30
+
+puts 5.5 * 5.5
+puts 623.1 * 623.1
+puts 25.1 * 25.1
+
+puts "." * 30
+
